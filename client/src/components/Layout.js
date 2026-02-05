623028e184f99b099045c9127713a4b6c45f9ee5
Mayur303 <mgccomputer2311@gmail.com>
2026-02-05 13:25:52 +0530

    Navbar updated making dynamic

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -3,16 +3,28 @@ import Navbar from "./Navbar";
 function Layout({ children }) {
   return (
     <>
+      {/* hover zone */}
+      <div style={styles.hoverZone} />
+
       <Navbar />
+
       <div style={styles.container}>{children}</div>
     </>
   );
 }
 
 const styles = {
+  hoverZone: {
+    position: "fixed",
+    top: 0,
+    height: "10px",
+    width: "100%",
+    zIndex: 998,
+  },
   container: {
-    minHeight: "calc(100vh - 60px)",
+    minHeight: "100vh",
     padding: "30px",
+    paddingTop: "40px",
   },
 };
 
