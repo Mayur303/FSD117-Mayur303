b5f4f585a29d7d328e60fd1c782462ee11af068e
Mayur303 <mgccomputer2311@gmail.com>
2026-02-11 15:07:33 +0530

    frontend ready

--- client/src/pages/Verifier.js
+++ client/src/pages/Verifier.js
@@ -314,18 +314,6 @@ const styles = {
   },
 };
 
-// Add keyframes for animations
-const globalStyles = `
-  @keyframes gradientShift {
-    0% { background-position: 0% 50%; }
-    50% { background-position: 100% 50%; }
-    100% { background-position: 0% 50%; }
-  }
-  @keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }
-`;
 
 export default Verifier;
 
